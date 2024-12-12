051e34342c3321cd19d809d9f29f09bd0cfa609a
itamarschnarch <itamar.schnarch@gmail.com>
2024-12-12 23:09:21 +0200

    validation checker fixes and redesign

--- validation_checker.py
+++ validation_checker.py
@@ -18,46 +18,81 @@ class ValidationChecker:
 
         char = expression[0]
         # invalid first char check
-        if not (char.isdigit() or ops_dict.get_priority(char) != -1 or char == '(' or char == ')') or char == 'u':
+        if (
+            not (
+                char.isdigit()
+                or ops_dict.get_priority(char) != -1
+                or char == "("
+                or char == ")"
+            )
+            or char == "u"
+        ):
             return False
 
         for i in range(len(expression)):
             char = expression[i]
 
-            if char == '(':
+            if char == "(":
                 left_bracket_counter += 1
-            if char == ')':
+            if char == ")":
                 right_bracket_counter += 1
 
-            if char == '.' and dot_flag == 1:
-                return False
-            if char == '.':
+            if char == ".":
                 dot_flag = 1
-            if char == expression[i-1] and not (char.isdigit() or char == '-' or char == '!' or char == '(' or char == ')'):
+            # 2 dots in a row
+            if dot_flag > 1:
+                return False
+            # dot before operator
+            if ops_dict.get_priority(char) != -1 and dot_flag == 1:
+                return False
+            # dot after operator
+            if ops_dict.get_priority(expression[i - 1]) != -1 and dot_flag == 1:
+                return False
+            # 2 operators in a row ( except '-','!','(',')' )
+            if char == expression[i - 1] and not (
+                char.isdigit()
+                or char == "-"
+                or char == "!"
+                or char == "("
+                or char == ")"
+            ):
                 return False
 
             # minus check
-            if char == '-':
-                if i == 0 or ops_dict.get_priority(expression[i-1]) != -1:
+            if char == "-":
+                if i == 0:
                     if i + 1 < len(expression):
                         next_char = expression[i + 1]
-                        if not (next_char.isdigit() or next_char == '-' or next_char == '('):
+                        while i + 1 < len(expression) and expression[i+1] == "-":
+                            i += 1
+                        if not (
+                            next_char.isdigit() or next_char == "-" or next_char == "("
+                        ):
                             return False
                     else:
                         return False
+                #elif (ops_dict.get_priority(expression[i - 1]) != -1):
+
             # tilda check
-            if char == '~':
-                    if i + 1 < len(expression):
-                        next_char = expression[i + 1]
-                        if not (next_char.isdigit() or next_char == '-'  or next_char == '('):
-                            return False
-                    else:
+            if char == "~":
+                if i + 1 < len(expression):
+                    next_char = expression[i + 1]
+                    if not (
+                        next_char.isdigit() or next_char == "-" or next_char == "("
+                    ):
                         return False
+                else:
+                    return False
             if ops_dict.get_priority(char) != -1:
                 dot_flag = 0
 
-         # last char is digit
-        if not (expression[-1].isdigit() or expression[-1] == '!' or expression[-1] == '#' or expression[-1] == ')'):
+        # last char is digit
+        if not (
+            expression[-1].isdigit()
+            or expression[-1] == "!"
+            or expression[-1] == "#"
+            or expression[-1] == ")"
+        ):
             return False
 
         # equal brackets check
@@ -65,10 +100,4 @@ class ValidationChecker:
             return False
         return True
 
-    # def are_valid_arithmetics_check(self, postfix_list_expression):
 
-
-    """
-vc = ValidationChecker()
-print(vc.are_valid_minus_tilda_expression("5+6"))
-    """
