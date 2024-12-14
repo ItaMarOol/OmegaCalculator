4e6069ba498df3ac484a61c31c7f144818c4fbe8
itamarschnarch <itamar.schnarch@gmail.com>
2024-12-14 12:38:51 +0200

    feat: added operators placements class to facilitate future changes

--- operators_dicts.py
+++ operators_dicts.py
@@ -1,6 +1,6 @@
 from operators import *
 
-class OperatorsPriority:
+class OperatorsPriorities:
     def __init__(self):
         self.operators_dictionary = {'+' : 1, '-' : 1, '*' : 2, '/':2,
                                      'u':2.5, '^': 3, '%': 4, '$': 5,
@@ -8,6 +8,16 @@ class OperatorsPriority:
 
     def get_priority(self, operator):
         return self.operators_dictionary.get(operator, -1)
+
+
+class OperatorsPlacements:
+    def __init__(self):
+        self.operators_class_dictionary = {'+' : "Middle", '-' : "Middle", '*' : "Middle", '/':"Middle",
+                                     'u':"Left", '^': "Middle", '%': "Middle", '$': "Middle",
+                                     '&': "Middle", '@': "Middle", '~': "Left", '!': "Right", '#': "Right"}
+
+    def get_placement(self, operator):
+        return self.operators_class_dictionary.get(operator, -1)
 
 class OperatorsClasses:
     def __init__(self):
