438e57167241bc3c41a84c4b06ddf00885d2eeb0
itamarschnarch <itamar.schnarch@gmail.com>
2024-12-16 21:41:51 +0200

    feat: added operators menu

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 from calculator import Calculator
 
-calc = Calculator()
-exp = ""
-while exp != "omega>sigit":
-    exp = input("Enter expression (to finish enter 'omega>sigit'): ")
-    if exp == "omega>sigit":
-        break
-    else:
-        result = calc.calculate(exp)
-        if isinstance(result, int) or isinstance(result, float):
-            print("Result:" ,result)
+
+def showMenu():
+    print("Special operators dictionary:")
+    print("'$' - maximum (example: 3$9 = 9)")
+    print("'&' - minimum (example: 3$9 = 3)")
+    print("'@' - average (example: 3$9 = 6)")
+    print("'~' - negative (example: ~5 = -5)")
+    print("'!' - factorial (example: 4! = 1*2*3*4 = 24)")
+    print("'#' - sum of digits (example: 245# = 2+4+5 = 11)")
+
+
+class Main:
+    calc = Calculator()
+    exp = ""
+    print(
+        "Welcome the the Omega calculator! This calculator works as a regular calculator but has some special operators"
+    )
+    showMenu()
+    while exp != "omega>sigit":
+        print("\nTo finish enter 'omega>sigit', to get help enter 'help me'")
+        exp = input("Enter expression: ")
+        if exp == "omega>sigit":
+            break
+        if exp == "help me":
+            showMenu()
         else:
-            print(result)
-print("exited from calculator")
+            result = calc.calculate(exp)
+            if isinstance(result, int) or isinstance(result, float):
+                print("Result:", result)
+            else:
+                print(result)
+    print("exited from calculator")
